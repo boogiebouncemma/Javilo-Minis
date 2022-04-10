24bca0162e05ad4d4ecb73b3708409a0fabc5c99
Boogie <85059214+boogiebouncemma@users.noreply.github.com>
2022-04-10 16:46:13 +0200

    Update app.js
    Portfolio and navigation bar code added

--- app.js
+++ app.js
@@ -49,6 +49,96 @@ function current(n) {
   timeoutSS = setInterval(showSlides, intervalTime);
 }
 
+
+
+
+/* navbar */
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {navbarFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navbarFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+/*Modal/Ligthbox*/
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "flex";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var lightboxIndex = 1;
+showSlidesLightbox(lightboxIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlidesLightbox(lightboxIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlidesLightbox(lightboxIndex = n);
+}
+
+
+function showSlidesLightbox(n) {
+  var i;
+  var lSlides = document.getElementsByClassName("lightboxSlides");
+  if (n > lSlides.length) {lightboxIndex = 1}
+  if (n < 1) {lightboxIndex = lSlides.length}
+  for (i = 0; i < lSlides.length; i++) {
+    lSlides[i].style.display = "none";
+  }
+
+  lSlides[lightboxIndex-1].style.display = "block";
+
+}
+
+// Portfolio image animations
+// Remove the transition class
+/*
+let square = document.querySelector('.portfolio_img');
+square.classList.remove('portfolio_img_transition');
+
+// Create the observer, same as before:
+let observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      square.classList.add('portfolio_img_transition');
+      return;
+    }
+
+    square.classList.remove('portfolio_img_transition');
+  });
+});
+
+let target = '.portfolio_img';
+document.querySelectorAll(target).forEach((i) => {
+    if (i) {
+        observer.observe(i);
+    }
+});
+*/
 /*
 if slide index >2, set slide index to 0
 if slide index <0, set slide index to 2
@@ -96,3 +186,4 @@ function moveForward() {
 
 
 showSlides();
+showSlidesLightbox();
